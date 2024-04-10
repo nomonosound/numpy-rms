f0e59e79d267e6419e362fc9df38248e6c0b8da8
iver56 <iver56@hotmail.com>
2024-04-10 13:12:09 +0200

    Write an AVX2-enabled implementation

--- numpy_rms/__init__.py
+++ numpy_rms/__init__.py
@@ -14,19 +14,24 @@ def rms(a: NDArray, window_size: int) -> NDArray:
     if 0 in a.shape:
         raise ValueError("Cannot input empty array")
 
-    if a.dtype == np.dtype("float32"):
-        if a.flags["C_CONTIGUOUS"] or a.flags["F_CONTIGUOUS"]:
-            output_shape = list(a.shape)
-            output_shape[-1] = math.floor(a.shape[-1] / window_size)
-            output_array = np.zeros(shape=output_shape, dtype=a.dtype)
-            _numpy_rms.lib.rms(
-                _numpy_rms.ffi.cast("float *", a.ctypes.data),
-                a.size,
-                window_size,
-                _numpy_rms.ffi.cast("float *", output_array.ctypes.data),
-                output_array.size,
-            )
-            return output_array
+    if (
+        a.dtype == np.dtype("float32")
+        and a.ndim == 1
+        and (a.flags["C_CONTIGUOUS"] or a.flags["F_CONTIGUOUS"])
+        and window_size % 8 == 0
+    ):
+        output_shape = list(a.shape)
+        output_shape[-1] = math.floor(a.shape[-1] / window_size)
+        output_array = np.zeros(shape=output_shape, dtype=a.dtype)
+        _numpy_rms.lib.rms(
+            _numpy_rms.ffi.cast("float *", a.ctypes.data),
+            a.size,
+            window_size,
+            _numpy_rms.ffi.cast("float *", output_array.ctypes.data),
+            output_array.size,
+        )
+        return output_array
 
     from .fallback import rms_numpy
+
     return rms_numpy(a, window_size)
--- numpy_rms/_rms.c
+++ numpy_rms/_rms.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <stdbool.h>
 
-void rms(const float *a, size_t length, int window_size, float *rms_output, size_t output_length) {
+void rms_scalar(const float *a, size_t length, int window_size, float *rms_output, size_t output_length) {
     int i = 0;
     int window_end;
     double window_sum;
@@ -17,4 +17,33 @@ void rms(const float *a, size_t length, int window_size, float *rms_output, size
         rms_output[output_i] = sqrt(window_sum / window_size);
     }
     return;
+}
+
+void rms(const float *a, size_t length, int window_size, float *rms_output, size_t output_length) {
+    int i = 0;
+    double window_sum;
+    __m256 sum_vec;
+    __m256 vals;
+    __m256 vals_squared;
+    float temp[8];
+
+    for (int output_i = 0; output_i < output_length; output_i++) {
+        window_sum = 0.0;
+
+        sum_vec = _mm256_setzero_ps();
+
+        int window_end = i + window_size;
+        for (; i < window_end; i += 8) {
+            vals = _mm256_loadu_ps(a + i);
+            vals_squared = _mm256_mul_ps(vals, vals);
+            sum_vec = _mm256_add_ps(sum_vec, vals_squared);
+        }
+
+        _mm256_storeu_ps(temp, sum_vec);
+        for (int j = 0; j < 8; ++j) {
+            window_sum += temp[j];
+        }
+
+        rms_output[output_i] = sqrt(window_sum / window_size);
+    }
 }
