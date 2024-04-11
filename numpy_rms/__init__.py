b002efe8df56d6b64a2a848c12e4296284e274c6
iver56 <iver56@hotmail.com>
2024-04-11 10:19:03 +0200

    Add multithreaded function

--- numpy_rms/__init__.py
+++ numpy_rms/__init__.py
@@ -1,4 +1,6 @@
 import math
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import List
 
 import _numpy_rms
 import numpy as np
@@ -11,6 +13,13 @@ except ImportError:
 
 
 def rms(a: NDArray, window_size: int) -> NDArray:
+    """
+    Calculate RMS series for the given NumPy array.
+
+    :param a: NumPy array to process.
+    :param window_size: Window size for the RMS calculation.
+    :return: A NumPy array containing the RMS series.
+    """
     if 0 in a.shape:
         raise ValueError("Cannot input empty array")
 
@@ -34,3 +43,34 @@ def rms(a: NDArray, window_size: int) -> NDArray:
     from .fallback import rms_numpy
 
     return rms_numpy(a, window_size)
+
+
+def rms_multithreaded(
+    arrays: List[NDArray], window_size: int, num_threads: int
+) -> List[NDArray]:
+    """
+    Processes a list of NumPy arrays, calculating the RMS for each array in a multithreaded manner.
+
+    :param arrays: List of NumPy arrays to process.
+    :param window_size: Window size for the RMS calculation.
+    :param num_threads: Number of threads to use for processing.
+    :return: A list of NumPy arrays containing the RMS results, None for arrays that caused an exception.
+    """
+
+    def worker(a):
+        return rms(a, window_size)
+
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        futures = {executor.submit(worker, a): a for a in arrays}
+        results = []
+
+        for future in as_completed(futures):
+            array = futures[future]
+            try:
+                result = future.result()
+                results.append(result)
+            except ValueError as e:
+                print(f"Error processing array {array}: {e}")
+                results.append(None)
+
+    return results
--- tests/test_rms.py
+++ tests/test_rms.py
@@ -13,12 +13,14 @@ def test_rms_large_array(benchmark):
     assert rms[0] == pytest.approx(2886.3184)
     assert rms[1] == pytest.approx(7637.1353)
 
+
 def test_rms_numpy_fallback_large_array(benchmark):
     arr = np.arange(100_000_000, dtype=np.float32)
     rms = benchmark(rms_numpy, arr, window_size=5000)
     assert rms.shape == (20_000,)
     assert rms[0] == pytest.approx(2886.3184)
     assert rms[1] == pytest.approx(7637.1353)
+
 
 def test_not_divisible_by_window_size():
     arr = np.arange(155, dtype=np.float32)
@@ -27,6 +29,7 @@ def test_not_divisible_by_window_size():
     assert rms.shape == rms_numpy_fallback.shape
     assert_array_almost_equal(rms, rms_numpy_fallback)
 
+
 def test_rms_window_size_not_divisible_by_8():
     arr = np.arange(130, dtype=np.float32)
     rms = numpy_rms.rms(arr, window_size=13)
@@ -34,9 +37,31 @@ def test_rms_window_size_not_divisible_by_8():
     assert rms.shape == rms_numpy_fallback.shape
     assert_array_almost_equal(rms, rms_numpy_fallback)
 
+
 def test_rms_window_size_smaller_than_8():
     arr = np.arange(40, dtype=np.float32)
     rms = numpy_rms.rms(arr, window_size=4)
     rms_numpy_fallback = rms_numpy(arr, window_size=4)
     assert rms.shape == rms_numpy_fallback.shape
     assert_array_almost_equal(rms, rms_numpy_fallback)
+
+
+def test_rms_singlethreaded_8_large_arrays(benchmark):
+    arrays = [np.arange(5_000_000, dtype=np.float32) for _ in range(8)]
+    rms = benchmark(
+        numpy_rms.rms_multithreaded, arrays, window_size=5000, num_threads=1
+    )
+    assert len(rms) == 8
+    assert rms[0].shape == (1_000,)
+    assert rms[0][0] == pytest.approx(2886.3184)
+    assert rms[-1][1] == pytest.approx(7637.1353)
+
+def test_rms_multithreaded_8_large_arrays(benchmark):
+    arrays = [np.arange(5_000_000, dtype=np.float32) for _ in range(8)]
+    rms = benchmark(
+        numpy_rms.rms_multithreaded, arrays, window_size=5000, num_threads=4
+    )
+    assert len(rms) == 8
+    assert rms[0].shape == (1_000,)
+    assert rms[0][0] == pytest.approx(2886.3184)
+    assert rms[-1][1] == pytest.approx(7637.1353)
